d147cc95759a691032968cadae01fb760b5e0529
Marvel Ravindra Dioputra <64513644+rdmrvl@users.noreply.github.com>
2024-02-12 10:45:56 +0700

    TP1

--- LP1/python/Dpr.py
+++ LP1/python/Dpr.py
@@ -2,31 +2,37 @@
 # Saya Marvel Ravindra Dioputra [2200481] LatPrak1 python dalam Mata Kuliah DPBO 
 # untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin
 # */
+
 class Dpr:
+    # Konstruktor
     def __init__(self, id, name, bidang, partai):
         self.id = id
         self.name = name
         self.bidang = bidang
         self.partai = partai
 
+    # Metode getter dan setter untuk atribut id
     def get_id(self):
         return self.id
 
     def set_id(self, id):
         self.id = id
 
+    # Metode getter dan setter untuk atribut nama
     def get_name(self):
         return self.name
 
     def set_name(self, name):
         self.name = name
 
+    # Metode getter dan setter untuk atribut bidang
     def get_bidang(self):
         return self.bidang
 
     def set_bidang(self, bidang):
         self.bidang = bidang
 
+    # Metode getter dan setter untuk atribut partai
     def get_partai(self):
         return self.partai
 
